c2272b8442a4732f08575e7d42d459b495319f20
semgilo <9737935@qq.com>
2018-10-20 22:41:54 +0800

    move base-gui and style to text-editor

--- lisp/init-text-editor.el
+++ lisp/init-text-editor.el
@@ -1,3 +1,39 @@
+;;----------------------------------------------------------------------------
+;;  Theme
+;;----------------------------------------------------------------------------
+(require-package 'monokai-theme)
+(load-theme 'monokai t)
+
+;;----------------------------------------------------------------------------
+;; Open GUI features
+;;----------------------------------------------------------------------------
+(linum-mode t)
+
+
+;;----------------------------------------------------------------------------
+;; Suppress GUI features
+;;----------------------------------------------------------------------------
+(setq use-file-dialog nil)
+(setq use-dialog-box nil)
+(setq inhibit-startup-screen t)
+(setq inhibit-startup-echo-area-message t)
+;; ace-window switch widow quikly 
+
+
+;;----------------------------------------------------------------------------
+;; Show a marker in the left fringe for lines not in the buffer
+;;----------------------------------------------------------------------------
+(setq indicate-empty-lines t)
+
+
+;;----------------------------------------------------------------------------
+;; Window size and features
+;;----------------------------------------------------------------------------
+(when (fboundp 'tool-bar-mode)
+  (tool-bar-mode -1))
+(when (fboundp 'set-scroll-bar-mode)
+  (set-scroll-bar-mode nil))
+
 
 ;;----------------------------------------------------------------------------
 ;;  GUI 
@@ -52,6 +88,7 @@
 
 ;; find-file-in-project
 (require-package 'find-file-in-project)
+(setq ffip-project-root "~/Documents/git")
 ;; (defun semgilo/open-lua-project (path)
 ;;   (interactive "input path: ")
 
@@ -104,10 +141,14 @@
 (global-set-key [M-down] 'move-text-down)
 
 ;;----------------------------------------------------------------------------
-;;  Theme
+;; Stop C-z from minimizing windows under OS X
 ;;----------------------------------------------------------------------------
-(require-package 'monokai-theme)
-(load-theme 'monokai t)
+(defun semgilo/maybe-suspend-frame ()
+  (interactive)
+  (unless (and *is-a-mac* window-system)
+    (suspend-frame)))
+
+(global-set-key (kbd "C-z") 'semgilo/maybe-suspend-frame)
 
 
-(provide 'init-style)
+(provide 'init-text-editor)
